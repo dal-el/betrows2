bb592f152061cb61872ad54a42ac4943f26d1a54
dal-el <lefterisdalivigkas@gmail.com>
2026-09-15 03:41:54 +0300

    update data Tue 09/15/2026  3:41:54,20

--- push_to_github.bat
+++ push_to_github.bat
@@ -3,10 +3,19 @@ cd /d "%~dp0"
 echo Ενημερωση GitHub με τα τελευταια δεδομενα (BetRows)...
 echo.
 
+REM 1) Αντιγραφη των live Stoiximan JSONs απο τον fetcher στο betrows-app\live
+REM    (ετσι δουλευει και η τοπικη προβολη index.html με τα ιδια δεδομενα)
+if not exist "C:\SOCCER_BETROWS\betrows-app\live" mkdir "C:\SOCCER_BETROWS\betrows-app\live"
+robocopy "C:\SOCCER_BETROWS\betrows-fetcher\output" "C:\SOCCER_BETROWS\betrows-app\live" /MIR /NFL /NDL /NJH /NJS >nul
+
+REM 2) Αντιγραφη βασικων αρχειων + live φακελου μεσα στο local git repo
 copy /Y "C:\SOCCER_BETROWS\betrows-app\data.json" "data.json" >nul
 copy /Y "C:\SOCCER_BETROWS\betrows-app\index.html" "index.html" >nul
 copy /Y "C:\SOCCER_BETROWS\betrows-app\crest-map.json" "crest-map.json" >nul
+if not exist "live" mkdir "live"
+robocopy "C:\SOCCER_BETROWS\betrows-app\live" "live" /MIR /NFL /NDL /NJH /NJS >nul
 
+REM 3) Commit + push στο GitHub
 git add -A
 git commit -m "update data %date% %time%"
 git push
